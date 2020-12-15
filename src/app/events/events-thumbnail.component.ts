97573427196b152461f44bc45ee4eb8831b8a581
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-15 12:26:33 -0700

    Styling Components

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -11,11 +11,14 @@ import { Component, Input } from '@angular/core';
         <div>\${{ event.price }}</div>
         <div>
           <span>{{ event.location.address }}</span>
-          <span>&nbsp;</span>
-          <span>{{ event.location.city }}, {{ event.location.country }}</span>
+          <span class="pad-left">{{ event.location.city }}, {{ event.location.country }}</span>
         </div>
       </div>
-    `
+    `,
+    styles: [`
+      .pad-left { margin-left: 10px; }
+      .well div { color: #bbb; }
+    `]
 })
 export class EventsThumbnailComponent {
 
