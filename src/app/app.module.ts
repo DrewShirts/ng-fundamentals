3df510ea0d0561839236e802d4d973482c849c95
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-18 00:27:45 -0700

    Wrapping Third Party Services

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -6,6 +6,7 @@ import { EventsListComponent } from './events/events-list.component';
 import { EventsThumbnailComponent } from './events/events-thumbnail.component';
 import { NavBarComponent } from './nav/navbar.component';
 import { EventService } from './shared/event.service';
+import { ToastrService } from './common/toastr.service';
 
 @NgModule({
   imports: [
@@ -17,7 +18,7 @@ import { EventService } from './shared/event.service';
     EventsThumbnailComponent,
     NavBarComponent
   ],
-  providers: [EventService],
+  providers: [EventService, ToastrService],
   bootstrap: [EventsAppComponent]
 })
 export class AppModule { }
--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,5 +1,7 @@
+import {toBase64String} from '@angular/compiler/src/output/source_map';
 import { Component, OnInit } from '@angular/core';
 import { EventService } from '../shared/event.service';
+import { ToastrService } from '../common/toastr.service';
 
 @Component({
   selector: 'events-list',
@@ -10,7 +12,7 @@ import { EventService } from '../shared/event.service';
         <hr>
         <div class="row">
           <div *ngFor="let event of events" class="col-md-5">
-            <events-thumbnail [event]="event"></events-thumbnail>
+            <events-thumbnail (click)="handleThumbnailClick(event.name)" [event]="event"></events-thumbnail>
           </div>
         </div>
       </div>
@@ -19,10 +21,13 @@ import { EventService } from '../shared/event.service';
 export class EventsListComponent implements OnInit {
   events:any[];
 
-  constructor(private eventService: EventService) {}
+  constructor(private eventService: EventService, private toastr: ToastrService) {}
 
   ngOnInit() {
     this.events = this.eventService.getEvents();
   }
 
+  handleThumbnailClick(eventName) {
+    this.toastr.success(eventName);
+  }
 }
