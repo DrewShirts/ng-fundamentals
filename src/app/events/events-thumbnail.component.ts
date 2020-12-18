43d13b8d70f349ca62061a6003e6197c63d38e8d
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-17 23:05:59 -0700

    Interpolation, Property Bindings, and Expressions

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -16,9 +16,10 @@ import { Component, Input } from '@angular/core';
       </div>
     `,
   styles: [`
-      .pad-left { margin-left: 10px; }
-      .well div { color: #bbb; }
-    `]
+    .thumbnail { min-height: 210px; }
+    .pad-left { margin-left: 10px; }
+    .well div { color: #bbb; }
+  `]
 })
 export class EventsThumbnailComponent {
 
