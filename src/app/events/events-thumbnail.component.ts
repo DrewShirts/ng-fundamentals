c8823094e5ff9207c959dee9ae12fd27b7e15310
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-17 23:43:51 -0700

    Styling Components with ngClass

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -7,7 +7,7 @@ import { Component, Input } from '@angular/core';
       <div class="well hoverwell thumbnail">
         <h2>{{ event?.name }}</h2>
         <div>Date: {{ event?.date }}</div>
-        <div [ngSwitch]="event?.time">
+        <div class="well" [ngClass]="getStartTimeClass()" [ngSwitch]="event?.time">
           Time: {{ event?.time }}
           <span *ngSwitchCase="'8:00 am'">(Early Start)</span>
           <span *ngSwitchCase="'10:00 am'">(Late Start)</span>
@@ -24,6 +24,8 @@ import { Component, Input } from '@angular/core';
       </div>
     `,
   styles: [`
+    .green { color: #003300 !important; }
+    .bold { font-weight: bold; }
     .thumbnail { min-height: 210px; }
     .pad-left { margin-left: 10px; }
     .well div { color: #bbb; }
@@ -32,5 +34,20 @@ import { Component, Input } from '@angular/core';
 export class EventsThumbnailComponent {
 
   @Input() event:any;
+
+  getStartTimeClass() {
+    // const isEarlyStart = this.event && this.event.time === '8:00 am';
+    // return {green: isEarlyStart, bold: isEarlyStart };
+
+    // if (this.event && this.event.time === '8:00 am') {
+    //   return 'green bold'
+    // }
+    // return ''
+
+    if (this.event && this.event.time === '8:00 am') {
+      return ['green', 'bold']
+    }
+    return []
+  }
 
 }
