a672ff449eb98aab992601bf4f73a0e11fabbaa4
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-17 23:17:01 -0700

    Hiding and Showing Content with ngIf

--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -9,11 +9,11 @@ import { Component, Input } from '@angular/core';
         <div>Date: {{ event?.date }}</div>
         <div>Time: {{ event?.time }}</div>
         <div>Price: \${{ event?.price }}</div>
-        <div>
+        <div *ngIf="event?.location">
           <span>Location: {{ event?.location?.address }}</span>
           <span class="pad-left">{{ event?.location?.city }}, {{ event?.location?.country }}</span>
         </div>
-        <div>
+        <div *ngIf="event?.onlineUrl">
           Online URL: {{ event?.onlineUrl }}
         </div>
       </div>
