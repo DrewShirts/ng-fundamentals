74b40f9bb48a7fc818f4339f769cc0406ac6ad32
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-21 15:44:20 -0700

    Guarding Against Route De-activation

--- src/app/routes.ts
+++ src/app/routes.ts
@@ -4,9 +4,10 @@ import {EventsListComponent} from "./events/events-list.component";
 import { CreateEventComponent } from './events/create-event.component';
 import { Error404Component } from './errors/404.component';
 import { EventRouteActivator } from './events/event-route-activator.service';
+import {CanDeactivate} from '@angular/router/src/utils/preactivation';
 
 export const appRoutes:Routes = [
-  { path: 'events/new', component: CreateEventComponent },
+  { path: 'events/new', component: CreateEventComponent, canDeactivate: ['canDeactivateCreateEvent'] },
   { path: 'events', component: EventsListComponent },
   { path: 'events/:id', component: EventDetailsComponent, canActivate: [EventRouteActivator] },
   { path: '404', component: Error404Component },
