cefe2dc04ff5ebf4636ce269be0f505ca40930ef
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-22 00:05:42 -0700

    Using Models for Type Safety

--- src/app/events/events-list.component.ts
+++ src/app/events/events-list.component.ts
@@ -1,8 +1,8 @@
-import {toBase64String} from '@angular/compiler/src/output/source_map';
 import { Component, OnInit } from '@angular/core';
 import { EventService } from './shared/event.service';
 import { ToastrService } from '../common/toastr.service';
 import { ActivatedRoute } from '@angular/router';
+import { IEvent } from './shared/event.model';
 
 @Component({
   template: `
@@ -18,7 +18,7 @@ import { ActivatedRoute } from '@angular/router';
     `
 })
 export class EventsListComponent implements OnInit {
-  events:any;
+  events:IEvent[];
 
   constructor(private eventService: EventService, private toastr: ToastrService,
     private route: ActivatedRoute) {}
--- src/app/events/events-thumbnail.component.ts
+++ src/app/events/events-thumbnail.component.ts
@@ -1,4 +1,5 @@
 import { Component, Input } from '@angular/core';
+import { IEvent } from './shared/event.model';
 
 @Component({
   selector: 'events-thumbnail',
@@ -31,7 +32,7 @@ import { Component, Input } from '@angular/core';
 })
 export class EventsThumbnailComponent {
 
-  @Input() event:any;
+  @Input() event:IEvent;
 
   getStartTimeStyle():any {
     if (this.event && this.event.time === '8:00 am') {
