6235586949fdf5557bc694388ea6d094a82e6d58
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-23 23:39:03 -0700

    Passing Data out of a Child Component

--- src/app/events/shared/event.service.ts
+++ src/app/events/shared/event.service.ts
@@ -14,10 +14,15 @@ export class EventService {
     return subject;
   }
 
-  saveEvent(event) {
+  saveEvent(event:IEvent) {
     event.id = 999;
     event.sessions = [];
     EVENTS.push(event);
+  }
+
+  updateEvent(event:IEvent) {
+    let index:number = EVENTS.findIndex(x => x.id === event.id);
+    EVENTS[index] = event;
   }
 
   getEvent(id:number):IEvent {
