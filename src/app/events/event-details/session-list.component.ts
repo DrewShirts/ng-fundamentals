cccd131ae80e08417e89883c37eaaa64a71dd33a
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-24 11:21:58 -0700

    Filtering Data

--- src/app/events/event-details/session-list.component.ts
+++ src/app/events/event-details/session-list.component.ts
@@ -1,10 +1,29 @@
-import { Component, Input } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 import { ISession } from '../shared';
 
 @Component({
   selector: 'session-list',
   templateUrl: './session-list.component.html',
 })
-export class SessionListComponent {
+export class SessionListComponent implements OnChanges {
+  @Input() filterBy:string;
   @Input() sessions:ISession[];
+  visibleSessions:ISession[] = [];
+
+  ngOnChanges() {
+    if (this.sessions) {
+      this.filterSessions(this.filterBy);
+    }
+  }
+
+  filterSessions(filter) {
+    if (filter === 'all') {
+      // slice this way makes a new copy of the sessions object in visibleSessions
+      this.visibleSessions = this.sessions.slice(0);
+    } else {
+      this.visibleSessions = this.sessions.filter(session => {
+        return session.level.toLocaleLowerCase() === filter;
+      });
+    }
+  }
 }
