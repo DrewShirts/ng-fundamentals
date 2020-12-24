8fc2aa2ca5ff514f1f7ce975cc5843e46837df9b
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-24 00:20:30 -0700

    Multiple Slot Content Projection

--- src/app/common/collapsible-well.component.ts
+++ src/app/common/collapsible-well.component.ts
@@ -1,16 +1,17 @@
-import { Component, Input } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component({
   selector: 'collapsible-well',
   template: `
     <div (click)="toggleContent()" class="well pointable">
-      <h4 class="well-title">{{title}}</h4>
-      <ng-content *ngIf="visible"></ng-content>
+      <h4>
+        <ng-content select="[well-title]"></ng-content>
+      </h4>
+      <ng-content *ngIf="visible" select="[well-body]"></ng-content>
     </div>
   `
 })
 export class CollapsibleWellComponent {
-  @Input() title:string;
   visible:boolean = true;
 
   toggleContent() {
