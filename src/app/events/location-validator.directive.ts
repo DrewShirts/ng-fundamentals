dd964a779172643ac003de209a10a6ce5f1f1be2
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-26 14:07:03 -0700

    Adding a Validator to Angular's Validators

--- src/app/events/location-validator.directive.ts
+++ src/app/events/location-validator.directive.ts
@@ -1,8 +1,9 @@
 import { Directive } from '@angular/core';
-import { Validator, FormGroup } from '@angular/forms';
+import { Validator, FormGroup, NG_VALIDATORS } from '@angular/forms';
 
 @Directive({
   selector: '[validateLocation]',
+  providers: [{provide: NG_VALIDATORS, useExisting: LocationValidator, multi: true}]
 })
 export class LocationValidator implements Validator {
   validate(formGroup: FormGroup): { [key: string]: any } {
