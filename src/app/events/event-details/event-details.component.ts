62d257a3a3656ed4eace232a627c60330e0ef1d3
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-26 11:57:01 -0700

    Routing to the Same Component

--- src/app/events/event-details/event-details.component.ts
+++ src/app/events/event-details/event-details.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Params } from '@angular/router';
 import { EventService } from '../shared/event.service';
 import { IEvent, ISession } from '../shared/event.model';
 
@@ -18,8 +18,12 @@ export class EventDetailsComponent implements OnInit {
   sortBy: string = 'votes';
 
   constructor(private eventService:EventService, private route:ActivatedRoute) {}
+
   ngOnInit() {
-    this.event = this.eventService.getEvent(+this.route.snapshot.params['id']);
+    this.route.params.forEach((params: Params) => {
+      this.event = this.eventService.getEvent(+params['id']);
+      this.addMode = false;
+    });
   }
 
   addSession() {
