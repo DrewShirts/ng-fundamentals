c1d08bfe3147047a5f2c90481d2e1c7881f45fa7
Drew Shirts <shirtsdre@churchofjesuschrist.org>
2020-12-26 21:43:48 -0700

    Persisting Authentication Status Across Page Refreshes

--- src/app/user/auth.service.ts
+++ src/app/user/auth.service.ts
@@ -28,6 +28,16 @@ export class AuthService {
     return !!this.currentUser;
   }
 
+  checkAuthenticationStatus() {
+    this.http.get('/api/currentIdentity')
+    .pipe(tap(data => {
+      if (data instanceof Object) {
+        this.currentUser = <IUser>data;
+      }
+    }))
+    .subscribe();
+  }
+
   updateCurrentUser(firstName: string, lastName: string) {
     this.currentUser.firstName = firstName;
     this.currentUser.lastName = lastName;
